723f99d6df8b28de66605f168e470e3449397baf
ssoham <sohamsinha2016@gmail.com>
2024-08-05 14:52:46 -0700

    fix: add bounds on liquid level and fix JT valve updates

--- sc_rf_service/sc_rf_refactored_service.py
+++ sc_rf_service/sc_rf_refactored_service.py
@@ -1,8 +1,9 @@
 from asyncio import get_event_loop, sleep
+from datetime import datetime
 from random import randrange
 from typing import Callable, Optional, Dict
 
-from caproto import ChannelFloat
+from caproto import ChannelFloat, ChannelEnum, ChannelInteger, ChannelString
 from caproto.server import ioc_arg_parser, run
 from lcls_tools.superconducting.sc_cavity import Cavity
 from lcls_tools.superconducting.sc_cryomodule import Cryomodule
@@ -11,53 +12,89 @@ from simulacrum import Service
 
 
 class PropertyFloatChannel(ChannelFloat):
-    def __init__(self, putter: Callable=None, precision=0, **kwargs):
+    def __init__(self, putter: Callable = None, precision=0, **kwargs):
         super().__init__(precision=precision, **kwargs)
         self.putter = putter
 
     async def verify_value(self, value):
         value = await super().verify_value(value)
-        if self.putter is not None:
-            return await self.putter(value)
+        if self.putter:
+            await self.putter(value)
+        return value
+
 
 class SimulacrumCavity(Cavity):
     def __init__(self, cavity_num: int, rack_object: "Rack"):
         super().__init__(cavity_num, rack_object)
         self.channels = {self.aact_pv: PropertyFloatChannel(value=16.6, putter=self.aact_amp_change),
-                         self.pv_addr("Q0"): ChannelFloat(value=randrange(int(2.5e10), int(3.5e10), step=int(0.1e10)))}
+                         self.pv_addr("Q0"): ChannelFloat(value=randrange(int(2.5e10), int(3.5e10), step=int(0.1e10))),
+                         self.acon_pv: ChannelFloat(value=16.6, precision=2),
+                         self.ades_pv: ChannelFloat(value=16.6, precision=2),
+                         self.rf_mode_ctrl_pv: ChannelEnum(value=4,
+                                                           enum_strings=(
+                                                               "SELAP", "SELA", "SEL", "SEL Raw", "Pulse", "Chirp")),
+                         self.rf_mode_pv: ChannelEnum(value=0,
+                                                      enum_strings=(
+                                                          "SELAP", "SELA", "SEL", "SEL Raw", "Pulse", "Chirp")),
+                         self.rf_state_pv: ChannelEnum(value=1, enum_strings=("Off", "On")),
+                         self.ades_max_pv: ChannelFloat(value=21.0),
+                         self.rf_permit_pv: ChannelEnum(value=1, enum_strings=("RF inhibit", "RF allow")),
+                         self.calc_probe_q_pv: ChannelInteger(value=0),
+                         self.push_ssa_slope_pv: ChannelInteger(value=0),
+                         self.interlock_reset_pv: ChannelEnum(value=0, enum_strings=("", "Reset")),
+                         self.drive_level_pv: ChannelFloat(value=0.0),
+                         self.characterization_start_pv: ChannelInteger(value=0),
+                         self.characterization_status_pv: ChannelString(
+                             value=datetime.now().strftime("%Y-%m-%d-%H:%M:%S")),
+                         self.push_scale_factor_pv: ChannelInteger(value=0),
+                         self.stepper_temp_pv: ChannelFloat(value=35.0),
+                         self.detune_best_pv: ChannelInteger(value=randrange(-10000, 10000)),
+                         self.quench_latch_pv: ChannelEnum(value=0, enum_strings=("Ok", "Fault")),
+                         }
 
     async def aact_amp_change(self, value: float):
         cm = self.cryomodule
         cm.jt_stable_pos = cm.total_heat_load(amp_change={self.number: value})
         jt_man_pos = cm.channels[cm.jt_prefix + "MANPOS_RQST"].value
-        if jt_man_pos * 1.1 > cm.jt_stable_pos:
-            while jt_man_pos * 1.1 > cm.jt_stable_pos:
+        if jt_man_pos * 0.9 > cm.jt_stable_pos:
+            while jt_man_pos * 0.9 > cm.jt_stable_pos:
                 await cm.adjust_liquid_level()
-        elif jt_man_pos * 0.9 < cm.jt_stable_pos:
-            while jt_man_pos * 0.9 < cm.jt_stable_pos:
+        elif jt_man_pos * 1.1 < cm.jt_stable_pos:
+            while jt_man_pos * 1.1 < cm.jt_stable_pos:
                 await cm.adjust_liquid_level()
 
 
 class SimulacrumCM(Cryomodule):
-    def __init__(self, cryo_name: str, linac_object: "Linac", ):
+    def __init__(self, cryo_name: str, linac_object: "Linac"):
         super().__init__(cryo_name, linac_object)
         self.channels = {}
-        self.heater_setpoint = f"CPIC:CM{self.name}:0000:EHCV:MANPOS_RQST"
+        self.heater_prefix = f"CPIC:CM{self.name}:0000:EHCV:"
+        self.heater_setpoint = self.heater_prefix + "MANPOS_RQST"
+        self.heater_mode = self.heater_prefix + "MODE_STRING"
         self.channels[self.ds_level_pv] = ChannelFloat(value=93.0)
         self.channels[self.us_level_pv] = ChannelFloat(value=75.0)
         self.channels[self.jt_valve_readback_pv] = ChannelFloat(value=30.0)
         self.channels[self.jt_prefix + "MANPOS_RQST"] = PropertyFloatChannel(value=40.0, putter=self.jt_valve_update)
+        self.channels[self.jt_valve_readback_pv] = ChannelFloat(value=40.0)
         self.channels[self.heater_setpoint] = PropertyFloatChannel(value=24.0, putter=self.heater_setpoint_update)
-        self.jt_stable_pos = self.total_heat_load()
+        self.channels[self.heater_readback_pv] = ChannelFloat(value=24.0)
+        self.channels[self.heater_mode] = ChannelString(value="MANUAL")
+
+        self._jt_stable_pos: Optional[float] = None
+
+    @property
+    def jt_stable_pos(self):
+        if not self._jt_stable_pos:
+            self._jt_stable_pos = self.total_heat_load()
+        return self.jt_stable_pos
 
     def total_heat_load(self, heater_value: Optional[float] = None, amp_change: Optional[Dict[int, float]] = None):
         rf_heat = 0
-        for _, cavity in self.cavities.items():
+        for cavity in self.cavities.values():
             channels = cavity.channels
             aact = channels[cavity.aact_pv].value
-            if amp_change:
-                if cavity.number == list(amp_change.keys())[0]:
-                    aact = amp_change[cavity.number]
+            if amp_change and cavity.number == list(amp_change.keys())[0]:
+                aact = amp_change[cavity.number]
             rf_heat += (aact * 1e6) ** 2 / (1012 * channels[cavity.pv_addr("Q0")].value)
         heater = self.channels[self.heater_setpoint].value if not heater_value else heater_value
         return rf_heat + heater
@@ -70,30 +107,35 @@ class SimulacrumCM(Cryomodule):
 
         jt_setpoint = self.channels[self.jt_prefix + "MANPOS_RQST"].value
         curr = self.channels[self.ds_level_pv].value
-        if jt_setpoint * 1.1 > self.jt_stable_pos:
-            await self.channels[self.ds_level_pv].write(curr - LOSS_AMOUNT)
-            await sleep(SLEEP_AMOUNT)
-        elif jt_setpoint * 0.9 < self.jt_stable_pos:
-            await self.channels[self.ds_level_pv].write(curr + GAIN_AMOUNT)
+        if 70 <= curr <= 100:
+            new_amount = curr
+            if jt_setpoint * 0.9 > self.jt_stable_pos:
+                new_amount -= LOSS_AMOUNT
+            elif jt_setpoint * 1.1 < self.jt_stable_pos:
+                new_amount += GAIN_AMOUNT
+            await self.channels[self.ds_level_pv].write(new_amount)
             await sleep(SLEEP_AMOUNT)
 
-    async def jt_valve_update(self, instance, value: float):
-        if value * 1.1 > self.jt_stable_pos:
-            while value * 1.1 > self.jt_stable_pos:
-                await sleep(1)
-        elif value * 0.9 < self.jt_stable_pos:
-            while value * 0.9 < self.jt_stable_pos:
-                await sleep(1)
+    async def jt_valve_update(self, value: float):
+        await self.channels[self.jt_valve_readback_pv].write(value)
+        if value * 0.9 > self.jt_stable_pos:
+            while self.channels[self.jt_valve_readback_pv].value * 0.9 > self.jt_stable_pos:
+                await self.adjust_liquid_level()
+        elif value * 1.1 < self.jt_stable_pos:
+            while self.channels[self.jt_valve_readback_pv].value * 1.1 < self.jt_stable_pos:
+                await self.adjust_liquid_level()
 
     async def heater_setpoint_update(self, value: float):
         self.jt_stable_pos = self.total_heat_load(heater_value=value)
-        jt_man_pos = self.channels[self.jt_prefix + "MANPOS_RQST"].value
-        if jt_man_pos * 1.1 > self.jt_stable_pos:
-            while jt_man_pos * 1.1 > self.jt_stable_pos:
-                await self.adjust_liquid_level()
-        elif jt_man_pos * 0.9 < self.jt_stable_pos:
-            while jt_man_pos * 0.9 < self.jt_stable_pos:
-                await self.adjust_liquid_level()
+        if self.channels[self.heater_mode].value == "MANUAL":
+            await self.channels[self.heater_readback_pv].write(value)
+            jt_man_pos = self.channels[self.jt_prefix + "MANPOS_RQST"].value
+            if jt_man_pos * 0.9 > self.jt_stable_pos:
+                while self.channels[self.jt_prefix + "MANPOS_RQST"].value * 0.9 > self.jt_stable_pos:
+                    await self.adjust_liquid_level()
+            elif jt_man_pos * 1.1 < self.jt_stable_pos:
+                while self.channels[self.jt_prefix + "MANPOS_RQST"].value * 1.1 < self.jt_stable_pos:
+                    await self.adjust_liquid_level()
 
 
 class CavityService(Service):
